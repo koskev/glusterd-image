9836a613998852b8cf0631c84957b1a30ac29e55
Kevin <kevin@kokev.de>
2023-12-23 19:52:20 +0100

    Copy default config to gluster dir on startup

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ RUN apt-get update \
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/*
 
+# Preserve group etc for people who want to mount glusterd on the host
+RUN cp -r /var/lib/glusterd /var/lib/glusterd.default
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 # Fix stupid ptr
 sed -E -i 's/^hostname1=[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}\./hostname1=/g' /var/lib/glusterd/peers/*
 
+# Copy default config (groups/hooks) to (possibly mounted) gluster dir
+cp -r /var/lib/glusterd.default/ /var/lib/glusterd/
+
 /usr/sbin/glusterd -p /var/run/glusterd.pid -N -l /dev/stdout $@
