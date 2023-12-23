b571762fba8846d692956bad9f54112bf317ebe3
Kevin <kevin@kokev.de>
2023-12-23 20:05:58 +0100

    Copy hooks and groups explicitly

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 sed -E -i 's/^hostname1=[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}\./hostname1=/g' /var/lib/glusterd/peers/*
 
 # Copy default config (groups/hooks) to (possibly mounted) gluster dir
-cp -r /var/lib/glusterd.default/ /var/lib/glusterd/
+cp -r /var/lib/glusterd.default/groups /var/lib/glusterd/
+cp -r /var/lib/glusterd.default/hooks /var/lib/glusterd/
 
 /usr/sbin/glusterd -p /var/run/glusterd.pid -N -l /dev/stdout $@
